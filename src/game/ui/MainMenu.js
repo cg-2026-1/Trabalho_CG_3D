43f12f5509962ad1dcc8550bdee0676ac716265c
Guilherme Gaspar <guilhermevgaspar07@gmail.com>
2026-06-24 12:52:40 -0300

    changes for hpBar and spBar dont view in main menu and upgrades in statsBar and position of coords Camera

--- src/game/ui/MainMenu.js
+++ src/game/ui/MainMenu.js
@@ -1,44 +1,55 @@
 let menu;
 
+function setHUDVisible(visible) {
+  const display = visible ? "block" : "none";
+
+  document.getElementById("crosshair").style.display = display;
+  document.getElementById("info").style.display = display;
+  document.getElementById("lightInfo").style.display = display;
+
+  document.getElementById("playerStats").style.display = visible
+    ? "flex"
+    : "none";
+
+  document.getElementById("hudBottom").style.display = visible
+    ? "flex"
+    : "none";
+}
+
 export function initMainMenu(onPlay) {
+  menu = document.getElementById("mainMenu");
 
-    menu = document.getElementById('mainMenu');
+  const playBtn = document.getElementById("playBtn");
+  const howBtn = document.getElementById("howBtn");
+  const configBtn = document.getElementById("configBtn");
+  const modal = document.getElementById("howToPlay");
 
-    const playBtn = document.getElementById('playBtn');
-    const howBtn = document.getElementById('howBtn');
-    const configBtn = document.getElementById('configBtn');
-    const modal = document.getElementById('howToPlay');
+  playBtn.addEventListener("click", () => {
+    hideMenu();
 
-    playBtn.addEventListener('click', () => {
+    setHUDVisible(true);
 
-        hideMenu();
+    onPlay();
+  });
+  howBtn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+  });
 
-        document.getElementById('crosshair').style.display = 'block';
-        document.getElementById('info').style.display = 'block';
-        document.getElementById('lightInfo').style.display = 'block';
-        document.getElementById('hudBottom').style.display = 'flex';
+  document.getElementById("closeHowBtn").addEventListener("click", () => {
+    modal.classList.add("hidden");
+  });
 
-        onPlay();
-    });
-
-    howBtn.addEventListener('click', () => {
-        modal.classList.remove('hidden');
-    });
-
-    document.getElementById('closeHowBtn')
-        .addEventListener('click', () => {
-            modal.classList.add('hidden');
-        });
-
-    configBtn.addEventListener('click', () => {
-        alert('Configurações ainda não implementadas');
-    });
+  configBtn.addEventListener("click", () => {
+    alert("Configurações ainda não implementadas");
+  });
 }
 
 export function showMenu() {
-    menu.style.display = 'flex';
+  menu.style.display = "flex";
+  setHUDVisible(false);
 }
 
 export function hideMenu() {
-    menu.style.display = 'none';
+  menu.style.display = "none";
+  setHUDVisible(true);
 }
